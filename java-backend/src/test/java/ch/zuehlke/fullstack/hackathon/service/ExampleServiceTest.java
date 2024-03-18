065b800aa91a9808ed499ad8fe354bee1dc4ddeb
Tim Scheifler <tim.scheifler@zuehlke.com>
2024-03-18 15:03:28 +0100

    feat: Extend UnitTest

--- java-backend/src/test/java/ch/zuehlke/fullstack/hackathon/service/ExampleServiceTest.java
+++ java-backend/src/test/java/ch/zuehlke/fullstack/hackathon/service/ExampleServiceTest.java
@@ -6,8 +6,12 @@ import ch.zuehlke.fullstack.hackathon.model.SubmitResponseDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ExampleServiceTest {
 
@@ -32,10 +36,15 @@ class ExampleServiceTest {
 
     @Test
     void submit_successfully() {
+        String expected = "Licht eingeschaltet";
+
+        when(aiServiceMock.submit(any())).thenReturn(Optional.of(expected));
+
         SubmitResponseDto actual = exampleService.submit("Schalte das licht an");
 
         assertThat(actual).isNotNull();
         assertThat(actual.content()).isNotEmpty();
+        assertThat(actual.content()).isEqualTo(expected);
     }
 
 
