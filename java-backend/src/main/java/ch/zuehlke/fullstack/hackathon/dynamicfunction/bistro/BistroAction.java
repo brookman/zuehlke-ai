9242c0f4fc6968e2d62f58cc1bc01cb021b4e0cc
Tim Scheifler <tim.scheifler@zuehlke.com>
2024-03-19 15:30:13 +0100

    feat: ExtendBistroAction logic to read data from the provided JSON. - create MenuItem as the DO for Menu of a specific day

--- java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/dynamicfunction/bistro/BistroAction.java
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/dynamicfunction/bistro/BistroAction.java
@@ -1,6 +1,7 @@
 package ch.zuehlke.fullstack.hackathon.dynamicfunction.bistro;
 
 import ch.zuehlke.fullstack.hackathon.dynamicfunction.Action;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.theokanning.openai.completion.chat.ChatFunctionCall;
@@ -8,9 +9,13 @@ import com.theokanning.openai.completion.chat.ChatFunctionDynamic;
 import com.theokanning.openai.completion.chat.ChatFunctionProperty;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class BistroAction implements Action {
 
@@ -21,7 +26,7 @@ public class BistroAction implements Action {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode response = mapper.createObjectNode();
         response.put("weekday", weekday);
-        response.put("food", "Pizza von Dieci"); //TODO read data from DB(?) by weekday
+        response.put("food", getMenusByDay(weekday).toString());
         return new ChatMessage(ChatMessageRole.FUNCTION.value(), response.toString(), "bistro_action");
     }
 
@@ -38,5 +43,16 @@ public class BistroAction implements Action {
                         .required(true)
                         .build())
                 .build();
+    }
+
+    private List<MenuItem> getMenusByDay(String weekday) {
+        List<MenuItem> weeklyMenu = new ArrayList<>();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            weeklyMenu = mapper.readValue(new ClassPathResource("weekly_menu.json").getInputStream(), new TypeReference<>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return weeklyMenu.stream().filter(menu -> menu.getWeek_day().equals(weekday)).toList();
     }
 }
---
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/dynamicfunction/bistro/MenuItem.java
@@ -0,0 +1,23 @@
+package ch.zuehlke.fullstack.hackathon.dynamicfunction.bistro;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+public class MenuItem {
+    private String week_day;
+    private String menu_type;
+    private String name;
+    private List<String> ingredients;
+}
+
