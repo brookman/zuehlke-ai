f25c0ebabec2a7ad76611a7b3b04ae6097ab083b
Tim Scheifler <tim.scheifler@zuehlke.com>
2024-03-19 14:36:18 +0100

    feat: add mock imgUrl at the last message

--- java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/websocket/WebSocketHandler.java
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/websocket/WebSocketHandler.java
@@ -40,7 +40,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     private TextMessage buildPayload(int messageId, WebsocketMessage websocketMessage) {
         if (websocketMessage.endMessage()) {
-            var response = "{ \"messageId\": " + messageId + ", \"chunk\": " + websocketMessage.content() + " }";
+            String imgUrl = "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg";
+            var response = "{ \"messageId\": " + messageId + ", \"imgUrl\": " + imgUrl + ", \"chunk\": " + websocketMessage.content() + " }";
             log.info(response);
             return new TextMessage(response);
         }
