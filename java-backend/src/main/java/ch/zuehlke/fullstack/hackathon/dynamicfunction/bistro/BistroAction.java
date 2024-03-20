53b7cbfe8dc7c8fa6f3cc614038284ae1ebaf570
Tim Scheifler <tim.scheifler@zuehlke.com>
2024-03-20 11:24:55 +0100

    refactor: Add relative day for requests

--- java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/dynamicfunction/bistro/BistroAction.java
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/dynamicfunction/bistro/BistroAction.java
@@ -10,14 +10,18 @@ import com.theokanning.openai.completion.chat.ChatFunctionDynamic;
 import com.theokanning.openai.completion.chat.ChatFunctionProperty;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 public class BistroAction implements Action {
 
     private static final String ACTION = "bistro_action";
@@ -32,9 +36,24 @@ public class BistroAction implements Action {
     public ChatMessageWrapper execute(ChatFunctionCall functionCall) {
         String weekday = functionCall.getArguments().get(WEEKDAY).asText();
         boolean vegetarian = functionCall.getArguments().get("vegetarian").asBoolean();
+        String relativeDay = functionCall.getArguments().get("relative_day").asText();
 
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode response = mapper.createObjectNode();
+
+        if (relativeDay != null) {
+            log.info("Get relative date");
+            LocalDate date = LocalDate.now();
+            int dayOfWeek = date.getDayOfWeek().getValue();
+            if (relativeDay.equals(RelativeDay.YESTERDAY.name())) {
+                dayOfWeek--;
+            } else if(relativeDay.equals(RelativeDay.TOMORROW.name())) {
+                dayOfWeek++;
+            }
+            weekday = Weekday.getWeekday(dayOfWeek).toString();
+            log.info("Got relative date: " + weekday);
+
+        }
         response.put(WEEKDAY, weekday);
         List<String> menusOfTheRequestedDay = getMenusByDay(weekday, vegetarian);
         response.put("food", menusOfTheRequestedDay.toString());
@@ -58,6 +77,13 @@ public class BistroAction implements Action {
                         .name("vegetarian")
                         .type("boolean")
                         .description("Either being true or false to decide if the vegetarian meal should be extracted or meat")
+                        .required(true)
+                        .build())
+                .addProperty(ChatFunctionProperty.builder()
+                        .name("relative_day")
+                        .type("string")
+                        .description("The relative day like today, tomorrow, yesterday")
+                        .enumValues(new HashSet<>(Arrays.stream(RelativeDay.values()).map(RelativeDay::name).toList()))
                         .required(true)
                         .build())
                 .build();
@@ -84,6 +110,29 @@ public class BistroAction implements Action {
         WEDNESDAY,
         THURSDAY,
         FRIDAY,
-        UNKNOWN
+        UNKNOWN;
+
+        public static Weekday getWeekday(int dayNumber) {
+            switch (dayNumber) {
+                case 1:
+                    return Weekday.MONDAY;
+                case 2:
+                    return Weekday.TUESDAY;
+                case 3:
+                    return Weekday.WEDNESDAY;
+                case 4:
+                    return Weekday.THURSDAY;
+                case 5:
+                    return Weekday.FRIDAY;
+                default:
+                    return Weekday.UNKNOWN;
+            }
+        }
+    }
+
+    enum RelativeDay {
+        YESTERDAY,
+        TODAY,
+        TOMORROW
     }
 }
